8e2e018ceddbfd7d10ef158fa06a619fd2d6c686
sheik195 <sheikalaudeen199@gmail.com>
2023-07-11 10:43:57 +0530

    forth commit

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-const express=require("express");
-const cors = require('cors');
-const app=express();
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-const login=require("./Routes/login")
-const admin=require('./Routes/admin');
-const review=require("./Routes/review");
-const order=require("./Routes/order");
-app.use(express.json())
-app.use(cors({
-    origin:"https://tubular-pixie-28fcd4.netlify.app",
-    credentials: true
-}))
+const login = require("./Routes/login");
+const admin = require("./Routes/admin");
+const review = require("./Routes/review");
+const order = require("./Routes/order");
 
-app.use("/login",login);
-app.use("/admin",admin);
-app.use("/review",review);
-app.use("/order",order);
+app.use(express.json());
 
-app.listen("3500",()=>{
-    console.log("server Running");
-})
+app.use(
+  cors({
+    origin: "https://tubular-pixie-28fcd4.netlify.app",
+    credentials: true,
+  })
+);
+
+app.use("/login", login);
+app.use("/admin", admin);
+app.use("/review", review);
+app.use("/order", order);
+
+app.listen(3500, () => {
+  console.log("Server running on port 3500");
+});
